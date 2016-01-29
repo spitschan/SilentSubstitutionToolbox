9727a33359569787b626965b6ce4c85bad2dc1d0
Manuel Spitschan <mspits@sas.upenn.edu>
2016-01-29 14:31:21 -0500

    Add GetSmithPokornyCones10
    Wrapper to get the linear transformation of CIE 1964 CMFs and return Smith-Pokorny 10° estimates of cone spectral sensitivities.
    
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ PhotoreceptorSensitivity/GetSmithPokornyCones10.m
@@ -0,0 +1,44 @@
+function [T_cones_sp10,S_cones_sp10] = GetSmithPokornyCones10(S,normalize)
+% [T_cones_sp10,S_cones_sp10] = GetSmithPokornyCones10(S,[normalize])
+%
+% Cao et al. write that they use the spectral sensitivities from Shapiro et
+% al. (1996). They in turn use the 1964 10° color-matching functions and then apply
+% a transformation to map to cone fundamentals to it. This transformation
+% matrix is given in Table 5. Below, this transformation matrix is called M.
+%
+% Optional argument normalize is false by default.  If set to true, each
+% cone sensitivity is normalized to a maximumu of 1.
+%
+% 10/24/15  dhb  Made it a function.
+
+% Set normalization
+if (nargin < 2 | isempty(normalize))
+    normalize = false;
+end
+
+% Set M matrix reconstruct the photoreceptor sensitivities they are using.
+M = [0.15516 0.54308 -0.03287 ; -0.15516 0.45692 0.03287 ; 0 0 1];
+
+% Load the 1964 10° CMFs
+wls = (380:1:780)'; S = WlsToS(wls);
+load T_xyz1964
+
+% Spline as necessary
+if (nargin < 1 | isempty(S))
+    S = S_xyz1964;
+else
+    T_xyz1964 = SplineCmf(S_xyz1964,T_xyz1964,S);
+    S_xyz1964 = S;
+end
+
+T_cones_sp10 = M*T_xyz1964;
+S_cones_sp10 = S;
+
+% Optional normalize
+if (normalize)
+    for i = 1:size(T_cones_sp10,1)
+        T_cones_sp10(i,:) = T_cones_sp10(i,:)/max(T_cones_sp10(i,:));
+    end
+end
+
+end
