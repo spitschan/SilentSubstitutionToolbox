d9c8a3b02c12d60be59b924fc02186399057a958
Manuel Spitschan <mspits@sas.upenn.edu>
2016-02-04 12:58:28 -0500

    Added comments
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- Nomograms/ShiftFundamental.m
+++ Nomograms/ShiftFundamental.m
@@ -1,2 +1,9 @@
 function out = ShiftFundamental(wls, in, shift);
+% out = ShiftFundamental(wls, in, shift);
+%
+% This function takes a spectral sensitivity (or any function as a a
+% function of wavelength) and shifts it according to shift.
+%
+% 2/4/16    ms      Wrote it.
+
 out = interp1(wls+shift, in, wls, 'spline');
