09385142af0015962b8ad1ace762b9e7b36ce4cb
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-21 10:11:49 -0400

    Also produce post-receptoral contrasts
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- ConeContrastCalcs/ComputeAndReportContrastsFromSpds.m
+++ ConeContrastCalcs/ComputeAndReportContrastsFromSpds.m
@@ -1,13 +1,23 @@
-function contrasts = ComputeAndReportContrastsFromSpds(string,photoreceptorClasses,T_receptors,backgroundSpd,modulationSpd,print)
-% contrasts = ComputeAndReportContrastsFromSpds(string,photoreceptorClasses,T_receptors,backgroundSpd,modulationSpd,print)
+function [contrasts postreceptoralContrasts] = ComputeAndReportContrastsFromSpds(string,photoreceptorClasses,T_receptors,backgroundSpd,modulationSpd,postreceptoralCombinations,print)
+% contrasts = ComputeAndReportContrastsFromSpds(string,photoreceptorClasses,T_receptors,backgroundSpd,modulationSpd,postreceptoralCombinations,print)
 %
 % Report out contrasts function. Assumes that the modulationSpd is the spd
 % measured around the background.
 %
 % See also ComputeAndReportContrastsFromOLPrimaries.
 
-if (nargin < 6 | isempty(print))
+% Do some check on the input
+if (nargin < 7 | isempty(print))
     print = true;
+end
+
+if (nargin < 6 | isempty(postreceptoralCombinations))
+    postreceptoralCombinations = [];
+end
+
+% Throw an error
+if size(postreceptoralCombinations, 2) ~= size(T_receptors, 1)
+    error('Postreceptoral combinations are not well specified. Check dimensions.');
 end
 
 backgroundReceptors = T_receptors*backgroundSpd;
@@ -17,4 +27,29 @@ if (print)
     for j = 1:size(T_receptors,1)
         fprintf('\t%s, <strong>%s</strong>: contrast = %0.1f%%\n',string,photoreceptorClasses{j},100*contrasts(j));
     end
+end
+
+% Let this breathe
+fprintf('\n');
+
+% Print out postreceptoral contrasts
+NCombinations = size(postreceptoralCombinations, 1);
+postreceptoralContrasts = postreceptoralCombinations' \ contrasts;
+for ii = 1:NCombinations
+    % Assemble the string
+    theReceptors = find(postreceptoralCombinations(ii, :));
+    theString = [];
+    for jj = 1:length(theReceptors)
+        if jj > 1
+            if sign(postreceptoralCombinations(ii, theReceptors(jj))) == 1
+                theString = [theString ' + ' photoreceptorClasses{theReceptors(jj)}];
+            elseif sign(postreceptoralCombinations(ii, theReceptors(jj))) == -1
+                theString = [theString ' - ' photoreceptorClasses{theReceptors(jj)}];
+            end
+        else
+            theString = [theString photoreceptorClasses{theReceptors(jj)}];
+        end
+    end
+    % Print out the contrasts
+    fprintf('\t%s, <strong>%s</strong>: contrast = %0.1f%%\n',string,theString,100*postreceptoralContrasts(ii));
 end
