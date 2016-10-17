db9275a940c25db1719bf11faae681d134a18ffc
Manuel Spitschan <mspits@sas.upenn.edu>
2016-10-17 12:21:29 -0400

    ENH: Also print out chromaticity
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- ContrastSplatter/GetLuminanceAndTrolandsFromSpd.m
+++ ContrastSplatter/GetLuminanceAndTrolandsFromSpd.m
@@ -9,6 +9,7 @@ function [photopicLuminanceCdM2, irradianceScotTrolands] = GetLuminanceAndTrolan
 load T_xyz1931
 T_xyz = SplineCmf(S_xyz1931,683*T_xyz1931,S);
 photopicLuminanceCdM2 = T_xyz(2,:)*radianceWattsPerM2Sr;
+chromaticityXY = T_xyz(1:2,:)*radianceWattsPerM2Sr/sum(T_xyz*radianceWattsPerM2Sr);
 
 % Calculate scotopic and photopic trolands
 pupilAreaMm2 = pi*((pupilDiameterMm/2)^2);
@@ -19,7 +20,8 @@ irradiancePhotTrolands = RetIrradianceToTrolands(irradianceWattsPerUm2, S, 'Phot
 irradianceScotTrolands = RetIrradianceToTrolands(irradianceWattsPerUm2, S, 'Scotopic', [], num2str(eyeLengthMm));
 
 if print
-    fprintf('  * Luminance <strong>%0.1f</strong> cd/m2\n',photopicLuminanceCdM2);
+    fprintf('  * Luminance <strong>%0.6f</strong> cd/m2\n',photopicLuminanceCdM2);
+    fprintf('  *   x=<strong>%0.2f</strong>, y=<strong>%0.2f</strong>\n',chromaticityXY(1), chromaticityXY(2));
     fprintf('  * Retinal irradiance [%g mm pupil] <strong>%0.1f</strong> ph td (<strong>%0.1f</strong> log ph td)\n',pupilDiameterMm,irradiancePhotTrolands,log10(irradiancePhotTrolands));
     fprintf('  * Retinal irradiance [%g mm pupil] <strong>%0.1f</strong> sc td (<strong>%0.1f</strong> log sc td)\n',pupilDiameterMm,irradianceScotTrolands, log10(irradianceScotTrolands)); 
 end
