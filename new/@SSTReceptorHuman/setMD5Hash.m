cee9e5803e1113b3c2321156f97023b176083c65
Manuel Spitschan <spitschan@stanford.edu>
2017-04-24 06:45:33 -0700

    Remove half-baked functions
    Signed-off-by: Manuel Spitschan <spitschan@stanford.edu>

--- new/@SSTReceptorHuman/setMD5Hash.m
+++
@@ -1,9 +0,0 @@
-function obj = setMD5Hash(obj, varargin)
-
-if isempty(obj.MD5Hash)
-    tmp = DataHash(obj);
-    if strcmp(obj.verbosity, 'high')
-       fprintf('* Setting hash: <strong>%s</strong>\n', tmp);
-    end
-    obj.MD5Hash = tmp;
-end
--- new/DefaultReceptorColors.m
+++
@@ -1,7 +0,0 @@
-function theRGB = DefaultReceptorColors
-% theRGB = DefaultReceptorColors
-
-rgbL = [0.8941    0.1020    0.1098];
-rgbM = [0.3020    0.6863    0.2902];
-rgbS = [0.2157    0.4941    0.7216];
-theRGB = [rgbL ; rgbM ; rgbS];
