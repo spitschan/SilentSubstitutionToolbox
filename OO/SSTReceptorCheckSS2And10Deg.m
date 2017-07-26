11268c06fd338353f8b118ad8ddf871c234c7ae7
Manuel Spitschan <spitschan@stanford.edu>
2017-07-26 12:29:04 +0200

    More comments

--- OO/SSTReceptorCheckSS2And10Deg.m
+++ OO/SSTReceptorCheckSS2And10Deg.m
@@ -1,7 +1,7 @@
 % SSTReceptorCheckSS2And10Deg.m
 %
 % Checks whether the @SSTReceptorHuman object returns the correct spectral
-% sensitivities by comparing them against the 2° and 10° Stockman-Sharpe
+% sensitivities by comparing them against the 2 deg and 10 deg Stockman-Sharpe
 % cone fundamentals.
 %
 % 7/26/17   ms      Written.
@@ -9,7 +9,7 @@
 % Wavelength sampling
 S = [380 5 81]; wls = SToWls(S);
 
-%% 2° cone fundamentals
+%% 2 deg cone fundamentals
 % Load from PTB
 targetRaw = load('T_cones_ss2');
 T_targetEnergy2 = SplineCmf(targetRaw.S_cones_ss2,targetRaw.T_cones_ss2,S,2);
@@ -19,7 +19,9 @@ for i = 1:3
     T_targetQuantal2(i,:) = T_targetQuantal2(i,:)/max(T_targetQuantal2(i,:));
 end
 
-% Construct with @SSTReceptorHuman 
+% Construct with @SSTReceptorHuman. The Stockman-Sharpe 2 deg fundamentals
+% assume an observer of 32 years, a pupil diameter of 3 mm, and a field
+% size of 2 deg.
 observerAgeInYrs = 32;
 pupilDiameterMm = 3;
 fieldSizeDegrees = 2;
@@ -38,7 +40,7 @@ xlabel('Wavelength [nm]'); ylabel('Normalized quantal sensitivity');
 title('2 deg fundamentals');
 xlim([380 780]);
 
-%% 10° cone fundamentals
+%% 10 deg cone fundamentals
 % Load from PTB
 load T_cones_ss10
 targetRaw = load('T_cones_ss10');
@@ -49,12 +51,15 @@ for i = 1:3
     T_targetQuantal10(i,:) = T_targetQuantal10(i,:)/max(T_targetQuantal10(i,:));
 end
 
-% Construct with @SSTReceptorHuman
+% Construct with @SSTReceptorHuman. The Stockman-Sharpe 2 deg fundamentals
+% assume an observer of 32 years, a pupil diameter of 3 mm, and a field
+% size of 10 deg.
 observerAgeInYrs = 32;
 pupilDiameterMm = 3;
 fieldSizeDegrees = 10;
 receptorObj = SSTReceptorHuman('S', S, 'obsAgeYrs', observerAgeInYrs, 'fieldSizeDeg', fieldSizeDegrees, 'obsPupilDiameterMm', pupilDiameterMm);
 
+% Plot
 subplot(1, 2, 2);
 h1 = plot(wls, receptorObj.T.T_quantalAbsorptionsNormalized', '-k', 'LineWidth', 4);
 hold on;
