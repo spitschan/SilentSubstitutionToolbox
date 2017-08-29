5ae352eddab356cf5511056c5f1aac7c97b8d603
Manuel Spitschan <spitschan@stanford.edu>
2017-08-29 10:53:23 +0100

    Updates

--- SST/unittests/SSTReceptorCheckAgreement.m
+++ SST/unittests/SSTReceptorCheckAgreement.m
@@ -8,31 +8,54 @@
 %
 % 8/29/17   ms      Written.
 
-% Iterate over
-for pupilDiameterMm = [2 4 6 8] % pupil diameter
-    for fieldSizeDeg = [2 10 20 40 60] % field size
-        for obsAgeInYrs = [20 30 40 50 60 70 80] % observer age
-            % [1] Spectral sensitivities throguh SSTReceptorHuman
-            receptorObj = SSTReceptorHuman('verbosity', 'low', 'fieldSizeDeg', fieldSizeDeg, ...
+% Define wavelength spacing
+S = [380 2 201];
+wls = SToWls(S);
+
+% Iterate over ...
+for pupilDiameterMm = [2 4 6 8] % ... pupil diameter
+    for fieldSizeDeg = [2 10 20 40 60] % ... field size
+        for obsAgeInYrs = [20 30 40 50 60 70 80] % ... observer age
+            % [1] Spectral sensitivities through SSTReceptorHuman
+            receptorObj = SSTReceptorHuman('S', S, 'verbosity', 'low', 'fieldSizeDeg', fieldSizeDeg, ...
                 'obsPupilDiameterMm', pupilDiameterMm, 'obsAgeInYrs', obsAgeInYrs, 'doPenumbralConesTrueFalse', true);
             
             % [2] Spectral sensitivities throguh GetHumanPhotoreceptorSS
-            [T_energyNormalized,T_quantalIsomerizations] = GetHumanPhotoreceptorSS([380 2 201], ...
+            [T_energyNormalized,T_quantalIsomerizations] = GetHumanPhotoreceptorSS(S, ...
                 {'LConeTabulatedAbsorbance' 'MConeTabulatedAbsorbance' 'SConeTabulatedAbsorbance' ...
                 'Melanopsin' 'Rods' ...
                 'LConeTabulatedAbsorbancePenumbral' 'MConeTabulatedAbsorbancePenumbral' 'SConeTabulatedAbsorbancePenumbral'}, ...
                 fieldSizeDeg, obsAgeInYrs, pupilDiameterMm);
-            %%
-            % Check for agreement
+            
+            % Check for agreement by calculating the sum of the absolute
+            % difference between the receptor sensitivities produced by
+            % SSTReceptorHuman and those porudced by
+            % GetHumanPhotoreceptorSS. These are in agreement, and would throw an error otherwise.
             for ii = 1:size(T_energyNormalized, 1)
                 difft0(ii) = sum(abs(receptorObj.T.T_energyNormalized(ii, :) - T_energyNormalized(ii, :)));
                 difft1(ii) = sum(abs(receptorObj.T.T_quantalIsomerizations(ii, :) - T_quantalIsomerizations(ii, :)));
-                if any(difft0 > 1e-7) || any(difft1 > 1e-7)
-                    error('X Not consistent');
-                else
-                    fprintf('X Pupil diam [mm] <strong>%d</strong>, field size [deg] <strong>%d</strong>, obs. age [yrs] <strong>%d</strong>: <strong>OK</strong>\n', pupilDiameterMm, fieldSizeDeg, obsAgeInYrs);
-                end
             end
+            if any(difft0 > 1e-7) || any(difft1 > 1e-7)
+                error('X Not consistent');
+            else
+                fprintf('X Pupil diam [mm] <strong>%d</strong>, field size [deg] <strong>%d</strong>, obs. age [yrs] <strong>%d</strong>: <strong>OK</strong>\n', pupilDiameterMm, fieldSizeDeg, obsAgeInYrs);
+            end
+            
+            % Make some plots
+            subplot(1, 2, 1);
+            plot(wls, receptorObj.T.T_energyNormalized, '-r', 'LineWidth', 2); hold on;
+            plot(wls, T_energyNormalized, '--k', 'LineWidth', 2);
+            xlabel('Wavelength [nm]'); ylabel('Sensitivity');
+            
+            subplot(1, 2, 2);
+            plot(wls, receptorObj.T.T_energyNormalized-T_energyNormalized, '-r'); hold on;
+            xlabel('Wavelength [nm]'); ylabel('\Delta Sensitivity');
+            pause;
+            
+            % Clear the drawing
+            subplot(1, 2, 1); hold off;
+            subplot(1, 2, 2); hold off;
+            
             clear difft0 difft1;
         end
     end
