66591cb4521cc1f1d1486538aa51d6fbd0b5a338
Manuel Spitschan <spitschan@stanford.edu>
2017-08-29 10:38:20 +0100

    Updated unit tests

--- SST/unittests/SSTReceptorCheckAgreement.m
+++ SST/unittests/SSTReceptorCheckAgreement.m
@@ -1,20 +1,29 @@
-%% Nominal ones
-pupilDiameterMm = 2;
-fieldSizeDeg = 50;
-obsAgeInYrs = 60;
-
-% [1] Spectral sensitivities throguh SSTReceptorHuman
-receptorObj = SSTReceptorHuman('verbosity', 'high', 'fieldSizeDeg', fieldSizeDeg, ...
-    'obsPupilDiameterMm', pupilDiameterMm, 'obsAgeInYrs', obsAgeInYrs);
-
-% [2] Spectral sensitivities throguh GetHumanPhotoreceptorSS
-[T_energyNormalized,T_quantalIsomerizations] = GetHumanPhotoreceptorSS([380 2 201], ...
-    {'LConeTabulatedAbsorbance' 'MConeTabulatedAbsorbance' 'SConeTabulatedAbsorbance' 'Melanopsin' 'Rods'}, ...
-    fieldSizeDeg, obsAgeInYrs, pupilDiameterMm);
-%%
-% Check for agreement
-fprintf('* Checking for agreement:\n');
-for ii = 1:size(T_energyNormalized, 1)
-    difft(ii) = sum(abs(receptorObj.T.T_energyNormalized(ii, :) - T_energyNormalized(ii, :)));
-    fprintf('    - <strong>%s</strong>:\t%d\n', receptorObj.labels{ii}, difft(ii));
+for pupilDiameterMm = [2:1:9];
+    for fieldSizeDeg = [1:1:60]
+        for obsAgeInYrs = [20:80];
+            % [1] Spectral sensitivities throguh SSTReceptorHuman
+            receptorObj = SSTReceptorHuman('verbosity', 'low', 'fieldSizeDeg', fieldSizeDeg, ...
+                'obsPupilDiameterMm', pupilDiameterMm, 'obsAgeInYrs', obsAgeInYrs, 'doPenumbralConesTrueFalse', true);
+            
+            % [2] Spectral sensitivities throguh GetHumanPhotoreceptorSS
+            [T_energyNormalized,T_quantalIsomerizations] = GetHumanPhotoreceptorSS([380 2 201], ...
+                {'LConeTabulatedAbsorbance' 'MConeTabulatedAbsorbance' 'SConeTabulatedAbsorbance' ...
+                'Melanopsin' 'Rods' ...
+                'LConeTabulatedAbsorbancePenumbral' 'MConeTabulatedAbsorbancePenumbral' 'SConeTabulatedAbsorbancePenumbral'}, ...
+                fieldSizeDeg, obsAgeInYrs, pupilDiameterMm);
+            %%
+            % Check for agreement
+            %fprintf('* Checking for agreement:\n');
+            for ii = 1:size(T_energyNormalized, 1)
+                difft0(ii) = sum(abs(receptorObj.T.T_energyNormalized(ii, :) - T_energyNormalized(ii, :)));
+                difft1(ii) = sum(abs(receptorObj.T.T_quantalIsomerizations(ii, :) - T_quantalIsomerizations(ii, :)));
+                if any(difft0 > 1e-7) || any(difft1 > 1e-7)
+                    error('Not consistent');
+                else
+                    %fprintf('    - <strong>%s</strong>:\t%d [energy], %d [isomer.]\n', receptorObj.labels{ii}, difft0(ii), difft1(ii));
+                end
+            end
+            clear difft0 difft1;
+        end
+    end
 end
