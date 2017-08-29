1de951935c4bc0f876729647157fb1fa18462e90
Manuel Spitschan <spitschan@stanford.edu>
2017-08-29 10:44:20 +0100

    Updates

--- SST/unittests/SSTReceptorCheckAgreement.m
+++ SST/unittests/SSTReceptorCheckAgreement.m
@@ -1,6 +1,17 @@
-for pupilDiameterMm = [2:1:9];
-    for fieldSizeDeg = [1:1:60]
-        for obsAgeInYrs = [20:80];
+%% SSTReceptorCheckAgreement
+%
+% This program checks for agreement between the old
+% 'GetHumanPhotoreceptorSS' method and the new 'SSTReceptorHuman' object by
+% serially iterating through various parameters sets (varying pupil
+% diameter, field size and observer age, which are the front-end
+% parameters).
+%
+% 8/29/17   ms      Written.
+
+% Iterate over
+for pupilDiameterMm = [2 4 6 8] % pupil diameter
+    for fieldSizeDeg = [2 10 20 40 60] % field size
+        for obsAgeInYrs = [20 30 40 50 60 70 80] % observer age
             % [1] Spectral sensitivities throguh SSTReceptorHuman
             receptorObj = SSTReceptorHuman('verbosity', 'low', 'fieldSizeDeg', fieldSizeDeg, ...
                 'obsPupilDiameterMm', pupilDiameterMm, 'obsAgeInYrs', obsAgeInYrs, 'doPenumbralConesTrueFalse', true);
@@ -13,14 +24,13 @@ for pupilDiameterMm = [2:1:9];
                 fieldSizeDeg, obsAgeInYrs, pupilDiameterMm);
             %%
             % Check for agreement
-            %fprintf('* Checking for agreement:\n');
             for ii = 1:size(T_energyNormalized, 1)
                 difft0(ii) = sum(abs(receptorObj.T.T_energyNormalized(ii, :) - T_energyNormalized(ii, :)));
                 difft1(ii) = sum(abs(receptorObj.T.T_quantalIsomerizations(ii, :) - T_quantalIsomerizations(ii, :)));
                 if any(difft0 > 1e-7) || any(difft1 > 1e-7)
-                    error('Not consistent');
+                    error('X Not consistent');
                 else
-                    %fprintf('    - <strong>%s</strong>:\t%d [energy], %d [isomer.]\n', receptorObj.labels{ii}, difft0(ii), difft1(ii));
+                    fprintf('X Pupil diam [mm] <strong>%d</strong>, field size [deg] <strong>%d</strong>, obs. age [yrs] <strong>%d</strong>: <strong>OK</strong>\n', pupilDiameterMm, fieldSizeDeg, obsAgeInYrs);
                 end
             end
             clear difft0 difft1;
