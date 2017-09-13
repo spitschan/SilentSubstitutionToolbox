d72593daf1bed474b1158a18d7f4d38d867685bb
Manuel Spitschan <spitschan@stanford.edu>
2017-09-13 13:20:33 +0100

    Updates

--- SST/ComputeCIEMelFundamental.m
+++ SST/ComputeCIEMelFundamental.m
@@ -1,10 +1,55 @@
 function [T_quantalAbsorptionsNormalizedMel,T_quantalAbsorptionsMel,T_quantalIsomerizationsMel,adjIndDiffParams,params,staticParams] = ComputeCIEMelFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
 % [T_quantalAbsorptionsNormalizedMel,T_quantalAbsorptionsMel,T_quantalIsomerizationsMel,params,staticParams] = ComputeCIEMelFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
 %
-% This function computes the melanopsin fundamental. It uses the same logic as
-% ComputeCIEConeFundamentals.
+% Usage:
+%     ComputeCIEMelFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
 %
-% 8/1/17    ms      Written.
+% Description:
+%     This function computes the melanopsin fundamental. It uses the same
+%     logic as ComputeCIEConeFundamentals.
+%
+% Input:
+%     S - Wavelength specification in standard PTB 'S' format
+%     fieldSizeDegrees - Field size in degrees
+%     ageInYears - Assumed observer age in years
+%     pupilDiameterMm - Assumed diameter in mm of the observer's pupil
+%     indDiffParams - struct containing the default individual difference
+%                     parameters in its field. These are:
+%                           indDiffParams.dlens - lens density
+%                           indDiffParams.dmac - macular pigment density
+%                           indDiffParams.dphotopigment - 1x3 vector of
+%                                                         photopigment
+%                                                         densities for LMS
+%                                                         cones
+%                           indDiffParams.lambdaMaxShift - 1x3 vector of
+%                                                          lambda max
+%                                                          shifts for LMS
+%                                                          cones
+%                           indDiffParams.shiftType - shift type for
+%                                                     lambda-max adjustment
+%     
+%
+% Output:
+%     T_quantalAbsorptionsNormalizedMel - Melanopsin normalized quantal
+%                                         absorptions in the same wavelength
+%                                         spacing as in S
+%     T_quantalAbsorptionsMel - Melanopsin quantal absorptions in the same
+%                               wavelength spacing as in S
+%     T_quantalIsomerizationsMel - Melanopsin quantal isomerizations in the
+%                                  same wavelength spacing as in S
+%     adjIndDiffParams - Individual difference parameters
+%     params, staticParams - Params structs returned from the DefaultPhotoreceptors and
+%                            FillInPhotoreceptors
+%
+% See also:
+%     ComputeCIERodFundamental, ComputeCIEConeFundamentals, ComputeRawConeFundamentals
+
+%
+% Optional key/value pairs:
+%     None.
+
+% 8/1/17    ms  Written.
+% 9/8/17    ms  Added header comments.
 
 %% Override default values so that FillInPhotoreceptors does
 % our work for us.  The CIE standard uses field size,
@@ -31,8 +76,5 @@ params.lambdaMax = photoreceptors.nomogram.lambdaMax;
 params.indDiffParams = indDiffParams;
 params.DORODS = true;
 
-%% Drop into more general routine to compute
-%
-% See comment in ComputeRawConeFundamentals about the fact that
-% we ought to unify this routine and what FillInPhotoreceptors does.
+%% Call ComputeRawConeFundamentals
 [T_quantalAbsorptionsNormalizedMel,T_quantalAbsorptionsMel,T_quantalIsomerizationsMel,adjIndDiffParams] = ComputeRawConeFundamentals(params,staticParams);
--- SST/ComputeCIERodFundamental.m
+++ SST/ComputeCIERodFundamental.m
@@ -1,10 +1,55 @@
 function [T_quantalAbsorptionsNormalizedRod,T_quantalAbsorptionsRod,T_quantalIsomerizationsRod,adjIndDiffParams,params,staticParams] = ComputeCIERodFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
-% [T_quantalAbsorptionsNormalizedRod,T_quantalAbsorptionsRod,T_quantalIsomerizationsRod,adjIndDiffParams,params,staticParams] = ComputeCIERodFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
+% [T_quantalAbsorptionsNormalizedRod,T_quantalAbsorptionsRod,T_quantalIsomerizationsRod,params,staticParams] = ComputeCIERodFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
 %
-% This function computes the rod fundamental. It uses the same logic as
-% ComputeCIEConeFundamentals.
+% Usage:
+%     ComputeCIERodFundamental(S,fieldSizeDegrees,ageInYears,pupilDiameterMm,indDiffParams)
 %
-% 8/1/17    ms      Written.
+% Description:
+%     This function computes the Rodanopsin fundamental. It uses the same
+%     logic as ComputeCIEConeFundamentals.
+%
+% Input:
+%     S - Wavelength specification in standard PTB 'S' format
+%     fieldSizeDegrees - Field size in degrees
+%     ageInYears - Assumed observer age in years
+%     pupilDiameterMm - Assumed diameter in mm of the observer's pupil
+%     indDiffParams - struct containing the default individual difference
+%                     parameters in its field. These are:
+%                           indDiffParams.dlens - lens density
+%                           indDiffParams.dmac - macular pigment density
+%                           indDiffParams.dphotopigment - 1x3 vector of
+%                                                         photopigment
+%                                                         densities for LMS
+%                                                         cones
+%                           indDiffParams.lambdaMaxShift - 1x3 vector of
+%                                                          lambda max
+%                                                          shifts for LMS
+%                                                          cones
+%                           indDiffParams.shiftType - shift type for
+%                                                     lambda-max adjustment
+%     
+%
+% Output:
+%     T_quantalAbsorptionsNormalizedRod - Rodanopsin normalized quantal
+%                                         absorptions in the same wavelength
+%                                         spacing as in S
+%     T_quantalAbsorptionsRod - Rodanopsin quantal absorptions in the same
+%                               wavelength spacing as in S
+%     T_quantalIsomerizationsRod - Rodanopsin quantal isomerizations in the
+%                                  same wavelength spacing as in S
+%     adjIndDiffParams - Individual difference parameters
+%     params, staticParams - Params structs returned from the DefaultPhotoreceptors and
+%                            FillInPhotoreceptors
+%
+% See also:
+%     ComputeCIEMelFundamental, ComputeCIEConeFundamentals, ComputeRawConeFundamentals
+
+%
+% Optional key/value pairs:
+%     None.
+
+% 8/1/17    ms  Written.
+% 9/8/17    ms  Added header comments.
 
 %% Override default values so that FillInPhotoreceptors does
 % our work for us.  The CIE standard uses field size,
@@ -31,8 +76,5 @@ params.lambdaMax = photoreceptors.nomogram.lambdaMax;
 params.indDiffParams = indDiffParams;
 params.DORODS = true;
 
-%% Drop into more general routine to compute
-%
-% See comment in ComputeRawConeFundamentals about the fact that
-% we ought to unify this routine and what FillInPhotoreceptors does.
+%% Call ComputeRawConeFundamentals
 [T_quantalAbsorptionsNormalizedRod,T_quantalAbsorptionsRod,T_quantalIsomerizationsRod,adjIndDiffParams] = ComputeRawConeFundamentals(params,staticParams);
