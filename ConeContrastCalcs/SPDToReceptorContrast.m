c502f4abd06db46785ff6195fee8efd278898c56
Joris Vincent <joris.vincent@pennmedicin.upenn.edu>
2017-12-01 16:48:47 -0500

    Header comment
    Updated header comment to conform to ISETBIO convention.

--- ConeContrastCalcs/SPDToReceptorContrast.m
+++ ConeContrastCalcs/SPDToReceptorContrast.m
@@ -1,34 +1,40 @@
 function [contrasts, response, responseDiff] = SPDToReceptorContrast(SPDs,SSTReceptors)
-% Calculates the contrast on each receptor between two SPDs
-%  
-% Description:
+% Calculates the contrast on photoreceptors between SPDs
 %
 % Syntax:
 %   contrasts = SPDtoReceptorContrast(SPDs,SSTReceptors)
 %
+% Description:
+%    SPDToReceptorContrast takes in spectra, as a set of columnvectors of
+%    power at wavelength for each spectrum, and takes in a set of
+%    receptors, as a SSTReceptor object, and calculates the contrast on
+%    each receptor between all pairs of spectra.
+%
 % Inputs:
-%   SPDs            nWlsxN Matrix of spectral power distributions, where
+%    SPDs         - nWlsxN Matrix of spectral power distributions, where
 %                   nWls is the wavelength specification, and N is the
 %                   number of spectra to calculate contrasts across
-%
-%   SSTReceptors    SSTReceptor object that specifies the receptors on
+%    SSTReceptors - SSTReceptor object that specifies the receptors on
 %                   which to calculate contrasts
 %
-% Optional key/value pairs:
-%
 % Outputs:
-%   contrasts       NxNxR matrix of contrasts (one NxN matrix per receptor 
+%    contrasts    - NxNxR matrix of contrasts (one NxN matrix per receptor 
 %                   type)
-%
-%   response        RxN matrix of responses of each receptor type to each 
+%    response     - RxN matrix of responses of each receptor type to each 
 %                   PSD
-%
-%   responseDiff    NxNxR matrix of differences in responses (one NxN 
+%    responseDiff - NxNxR matrix of differences in responses (one NxN 
 %                   matrix per receptor type)
+%
+% Optional key/value pairs:
+%   None.
 %
 % Notes:
 %
 % See also:
+
+% History:
+%    12/01/17  jv  created. based on ComputerAndReportContrastsFromSpds     
+%
      
     % Calculate receptor response
     response = SSTReceptors.T.T_energyNormalized * SPDs;
