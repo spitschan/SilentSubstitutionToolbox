d59f02f59a0f1520babd078082190ab4ec8a14b2
Manuel Spitschan <spitschan@stanford.edu>
2018-03-30 08:02:30 +0100

    Reverted until fix

--- ConeContrastCalcs/ComputeAndReportContrastsFromSpds.m
+++ ConeContrastCalcs/ComputeAndReportContrastsFromSpds.m
@@ -53,8 +53,9 @@ end
 if (p.Results.verbose),  fprintf('\n<strong>%s</strong>\n', prefixString); end
 
 %% Calculate the contrasts
-receptorContrasts = SPDToReceptorContrast([backgroundSpd, modulationSpd],T_receptors);
-receptorContrasts = receptorContrasts(:,1);
+%receptorContrasts = SPDToReceptorContrast([backgroundSpd, modulationSpd],T_receptors);
+%receptorContrasts = receptorContrasts(:,1)
+receptorContrasts = (T_Receptors*(modulationSpd-backgroundSpd))./(T_Receptors*backgroundSpd);
 
 %% Report
 if (p.Results.verbose)
