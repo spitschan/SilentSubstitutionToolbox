ec2961562a12c0c517070ef252ca4be70fcaf6b7
David Brainard <brainard@psych.upenn.edu>
2018-10-17 09:25:02 -0400

    Add options that let this do validations as well as run in interactive mode
    Infrastructure is now in place, but need to add actual check values.

--- ReceptorIsolate/ReceptorIsolateDemo.m
+++ ReceptorIsolate/ReceptorIsolateDemo.m
@@ -1,66 +1,114 @@
-% ReceptorIsolateDemo
+function status = ReceptorIsolateDemo(varargin)
+% Demonstrate calculation of receptor isolating directions
 %
-% Find modulations that isolates various photopigments, for various device
-% models. The engine that drives this demo is called ReceptorIsolate.
+% Syntax:
+%    ReceptorIsolateDemo
 %
-% This demonstrates various cases, including when you've just got a
-% standard three primary monitor and when you have a device that can
-% produce arbitrary (within gamut limits) spectra across the visible range
-% of wavelengths.
+% Description:
+%    Find modulations that isolates various photopigments, for various device
+%    models. The engine that drives this demo is called ReceptorIsolate.
 %
-% It also demonstrates our code for producing individually customized (for
-% age, field size, pupil diameter, and background light level) estimates of
-% human cone spectral sensitivities.
+%    This demonstrates various cases, including when you've just got a
+%    standard three primary monitor and when you have a device that can
+%    produce arbitrary (within gamut limits) spectra across the visible range
+%    of wavelengths.
 %
-% 3/30/12  dhb      Wrote it.
-% 2/3/13   ms       Updated to match changed ReceptorIsolate syntax.
-% 4/19/13  dhb, ms  Got this working again.
-% 12/11/14 dhb      Clean up for release.
-% 1/6/17   ms       Updated name convention for photopigments.
-% 08/10/18 dhb      Add L-M direction demo.  This works for a specified
-%                   contrast. I think we have code in
-%                   ReceptorIsolateOptimBackgroundMulti that allows
-%                   maximizing along a direction. For another day to
-%                   remember how that works.
+%    It also demonstrates our code for producing individually customized (for
+%    age, field size, pupil diameter, and background light level) estimates of
+%    human cone spectral sensitivities.
+%
+% Inputs:
+%    None
+%
+% Outputs:
+%    status - Returns 1 in interactive mode, or if validations pass. Returns
+%             0 if a validation fails.
+%
+% Optional key/value pairs:
+%    'validate'                - String (default 'none'). Can pass strings to run
+%                                preset validations. When string is none, runs in
+%                                interactive mode and prompts for values.
+%                                 - 'none' Interactive mode, no validation checks.
+%                                 - 'basichuman' Basic human validation
+%    'whichDirectionNumber'    - Number (default 1). Which preset direction
+%                                to validate, when 'validate' is not 'none'
+%
+% See also:
+%
+
+% History:
+%   3/30/12  dhb      Wrote it.
+%   2/3/13   ms       Updated to match changed ReceptorIsolate syntax.
+%   4/19/13  dhb, ms  Got this working again.
+%   12/11/14 dhb      Clean up for release.
+%   1/6/17   ms       Updated name convention for photopigments.
+%   08/10/18 dhb      Add L-M direction demo.  This works for a specified
+%                     contrast. I think we have code in
+%                     ReceptorIsolateOptimBackgroundMulti that allows
+%                     maximizing along a direction. For another day to
+%                     remember how that works.
+%   10/17/18 dhb      Add validation mode.  
 
 %% Clear and close
-clear; close all;
+close all;
+
+%% Parse input
+p = inputParser;
+p.addParameter('validate','none',@ischar);
+p.addParameter('whichDirectionNumber',1,@isnumeric);
+p.parse(varargin{:});
+
+%% Set status to OK
+status = 1;
 
 %% Run in the directory that contains this file.
+curDir = pwd;
 cd(fileparts(mfilename('fullpath')));
 
 %% User choice of which of several cases to do
-fprintf('Available receptor models:\n');
-fprintf('\t[1]  Human cones and melanopsin\n');
-fprintf('\t[2]  Dog\n');
-whichModelNumber = GetWithDefault('Enter model',1);
-switch (whichModelNumber)
-    case 1
+switch (p.Results.validate)
+    case 'basichuman'
         whichModel = 'HumanPhotopigments';
-    case 2
-        whichModel = 'Dog';
-    otherwise
-        error('Unknown receptor case entered');
-end
-
-%% Prompt for device to compute with respect to
-fprintf('\nAvailable devices:\n');
-fprintf('\t[1]  Spectral - ideal spectral producing device\n');
-fprintf('\t[2]  OneLight - calibration data from Brainard/Aguirre lab OneLight device\n');
-fprintf('\t[3]  Monitor - some typical monitor\n');
-fprintf('\t[4]  FiveLEDPrimaries - 5 LEDs\n');
-whichPrimaryNumber = GetWithDefault('Enter device',1);
-switch (whichPrimaryNumber)
-    case 1
         whichPrimaries = 'Spectral';
-    case 2
-        whichPrimaries = 'OneLight';
-    case 3
-        whichPrimaries = 'Monitor';
-    case 4
-        whichPrimaries = 'FiveLEDPrimaries';
+        basicHuman.maximizeTargetContrast = 1;
+        basicHuman.Results.desiredContrast = [];
+        
+    case {'none'}
+        % Interactive mode
+        fprintf('Available receptor models:\n');
+        fprintf('\t[1]  Human cones and melanopsin\n');
+        fprintf('\t[2]  Dog\n');
+        whichModelNumber = GetWithDefault('Enter model',1);
+        switch (whichModelNumber)
+            case 1
+                whichModel = 'HumanPhotopigments';
+            case 2
+                whichModel = 'Dog';
+            otherwise
+                error('Unknown receptor case entered');
+        end
+        
+        % Prompt for device to compute with respect to
+        fprintf('\nAvailable devices:\n');
+        fprintf('\t[1]  Spectral - ideal spectral producing device\n');
+        fprintf('\t[2]  OneLight - calibration data from Brainard/Aguirre lab OneLight device\n');
+        fprintf('\t[3]  Monitor - some typical monitor\n');
+        fprintf('\t[4]  FiveLEDPrimaries - 5 LEDs\n');
+        whichPrimaryNumber = GetWithDefault('Enter device',1);
+        switch (whichPrimaryNumber)
+            case 1
+                whichPrimaries = 'Spectral';
+            case 2
+                whichPrimaries = 'OneLight';
+            case 3
+                whichPrimaries = 'Monitor';
+            case 4
+                whichPrimaries = 'FiveLEDPrimaries';
+            otherwise
+                error('Unknown primaries entered');
+        end
     otherwise
-        error('Unknown primaries entered');
+        error('Unknown validation type specified');
 end
 
 %% Define primaries and conditions on them
@@ -198,13 +246,26 @@ switch (whichModel)
         % parameters but rather simply accept the defaults used by
         % GetHumanPhotoreceptorSS.  It's mainly for fun that we show
         % how to do this here.
-        observerAgeInYears = GetWithDefault('\tObserver age in years?', 32);
-        fieldSizeDegrees = GetWithDefault('\tField size in degrees?', 27.5);
-        pupilDiameterMm = GetWithDefault('\tPupil diameter?', 4.7);
-        vesselOxyFraction = GetWithDefault(['\tOxygenation fraction for vessel hemoglobin [typical 0.85]?'], 0.85);
-        vesselOverallThicknessUm = GetWithDefault(['\tVessel thickness [typical 5 um]?'], 5);
-        correctBleaching = GetWithDefault(['\tCorrect for cone photopigment bleaching [1 = yes, 0 = no]?'],0);
-        
+        switch (p.Results.validate)
+            case 'basichuman'
+                observerAgeInYears = 32;
+                fieldSizeDegrees = 27.5;
+                pupilDiameterMm = 4.7;
+                vesselOxyFraction = 0.85;
+                vesselOverallThicknessUm = 5;
+                correctBleaching = 0;
+            case {'none'}
+                % Interactive mode
+                observerAgeInYears = GetWithDefault('\tObserver age in years?', 32);
+                fieldSizeDegrees = GetWithDefault('\tField size in degrees?', 27.5);
+                pupilDiameterMm = GetWithDefault('\tPupil diameter?', 4.7);
+                vesselOxyFraction = GetWithDefault(['\tOxygenation fraction for vessel hemoglobin [typical 0.85]?'], 0.85);
+                vesselOverallThicknessUm = GetWithDefault(['\tVessel thickness [typical 5 um]?'], 5);
+                correctBleaching = GetWithDefault(['\tCorrect for cone photopigment bleaching [1 = yes, 0 = no]?'],0);
+            otherwise
+                error('Unknown validation type specified');
+        end
+
         % Define photoreceptor classes that we'll consider.
         % ReceptorIsolate has a few more built-ins than these.
         photoreceptorClasses = {'LConeTabulatedAbsorbance', 'MConeTabulatedAbsorbance', 'SConeTabulatedAbsorbance', 'Melanopsin', 'Rods', ...
@@ -259,15 +320,23 @@ switch (whichModel)
         
         %% Let user choose a photoreceptor class to target
         LMinusMTargetContrast = 0.06;
-        fprintf('Available photoreceptor classes to target:\n');
-        fprintf('\t[1]  Melanopsin, silence open-field cones; ignore rods and penumbral cones\n');
-        fprintf('\t[2]  Melanopsin, silence open-field and penumbral cones; ignore rods)\n');
-        fprintf('\t[3]  S cones, silence open-field L and M cones, melanopsin, and prenumbral L and M cones; ignore rods and penumbral S cones\n');
-        fprintf('\t[4]  S cones, silence open field L and M cones, ingore all others\n');
-        fprintf('\t[5]  Penumbral L and M cones, silence open-field cones, melanopsin, and prenumbral S cones; ignore rods\n');
-        fprintf('\t[6]  Rods\n');
-        fprintf('\t[7]  L minus M at %0.2f contrast\n',LMinusMTargetContrast);
-        whichDirectionNumber = GetWithDefault('Enter direction',1);
+        switch (p.Results.validate)
+            case 'basichuman'
+                whichDirectionNumber = p.Results.whichDirectionNumber;
+            case {'none'}
+                % Interactive
+                fprintf('Available photoreceptor classes to target:\n');
+                fprintf('\t[1]  Melanopsin, silence open-field cones; ignore rods and penumbral cones\n');
+                fprintf('\t[2]  Melanopsin, silence open-field and penumbral cones; ignore rods)\n');
+                fprintf('\t[3]  S cones, silence open-field L and M cones, melanopsin, and prenumbral L and M cones; ignore rods and penumbral S cones\n');
+                fprintf('\t[4]  S cones, silence open field L and M cones, ingore all others\n');
+                fprintf('\t[5]  Penumbral L and M cones, silence open-field cones, melanopsin, and prenumbral S cones; ignore rods\n');
+                fprintf('\t[6]  Rods\n');
+                fprintf('\t[7]  L minus M at %0.2f contrast\n',LMinusMTargetContrast);
+                whichDirectionNumber = GetWithDefault('Enter direction',1);
+            otherwise
+                error('Unknown validation type specified');
+        end
         
         % Depending on which direction is chosen, specify the indices
         % into the rows of T_receptors to define the various classes.
@@ -324,7 +393,7 @@ switch (whichModel)
         end
         
         
-    case 'Dog';
+    case 'Dog'
         % Load in PTB's view of the photoreceptor spectral sensitivities in
         % canine.
         load T_dogrec
@@ -372,12 +441,26 @@ end
 % This is not necessary, they can differ.  It just makes the demo code a
 % bit simpler to yoke them since we only have to prompt for one number.
 if (~exist('desiredContrast','var') | isempty(desiredContrast))
-    maximizeTargetContrast = GetWithDefault('\tMaximize contrast? [1 = yes, 0 = no]', 1);
-    if maximizeTargetContrast
-        desiredContrast = [];
-    else
-        desiredContrast = GetWithDefault('\tDesired contrast (applies to all targeted classes)?', 0.45)*ones(size(whichReceptorsToTarget));
-    end
+    switch (p.Results.validate)
+        case 'basichuman'
+            maximizeTargetContrast = basicHuman.maximizeTargetContrast;
+            if maximizeTargetContrast
+                desiredContrast = [];
+            else
+                desiredContrast = basicHuman.desiredContrast;
+            end
+            
+        case {'none'}
+            % Interactive mode
+            maximizeTargetContrast = GetWithDefault('\tMaximize contrast? [1 = yes, 0 = no]', 1); 
+            if maximizeTargetContrast
+                desiredContrast = [];
+            else
+                desiredContrast = GetWithDefault('\tDesired contrast (applies to all targeted classes)?', 0.45)*ones(size(whichReceptorsToTarget));
+            end
+        otherwise
+            error('Unknown validation type specified');
+    end   
 end
 
 % Nice message for user
@@ -425,45 +508,58 @@ for j = 1:size(T_receptors,1)
     fprintf('\t%s: contrast = %0.4f\n',photoreceptorClasses{j},contrastReceptors(j));
 end
 
-%% Plots
-plotDir = 'ReceptorIsolateDemoOutput';
-if ~isdir(plotDir)
-    mkdir(plotDir);
+%% Plots and/or validation check
+switch (p.Results.validate)
+    case 'none'
+        % Make lots in interactive mode
+        plotDir = 'ReceptorIsolateDemoOutput';
+        if ~isdir(plotDir)
+            mkdir(plotDir);
+        end
+        curDir = pwd;
+        cd(plotDir);
+        
+        % Photoreceptor sensitivities
+        theFig1 = figure; clf; hold on
+        plot(SToWls(S),T_receptors,'LineWidth',2);
+        xlabel('Wavelength (nm)')
+        ylabel('Sensitivity');
+        title('Normalized photoreceptor sensitivities');
+        saveas(theFig1,sprintf('%s_%s_%s_Sensitivities.pdf',whichModel,whichPrimaries,whichDirection),'pdf');
+        
+        % Modulation spectra
+        theFig2 = figure; hold on
+        plot(SToWls(S),B_primary*modulationPrimary,'r','LineWidth',2);
+        plot(SToWls(S),B_primary*backgroundPrimary,'k','LineWidth',2);
+        title('Modulation spectra');
+        xlim([380 780]);
+        xlabel('Wavelength');
+        ylabel('Power');
+        pbaspect([1 1 1]);
+        saveas(theFig2,sprintf('%s_%s_%s_Modulation.pdf',whichModel,whichPrimaries,whichDirection),'pdf');
+        
+        % Primaries
+        theFig3 = figure; hold on
+        plot(modulationPrimary,'r','LineWidth',2);
+        plot(backgroundPrimary+(-(modulationPrimary-backgroundPrimary)),'g','LineWidth',2);
+        plot(backgroundPrimary,'k','LineWidth',2);
+        title('Primary settings');
+        xlim([0 length(backgroundPrimary)]);
+        ylim([0 1]);
+        xlabel('Primary Number (nominal)');
+        ylabel('Setting');
+        legend({'Positive', 'Negative', 'Background'},'Location','NorthEastOutside');
+        saveas(theFig3,sprintf('%s_%s_%s_Primaries.pdf',whichModel,whichPrimaries,whichDirection),'pdf');
+    case 'basichuman'
+        % Check that some numbers computed now match what they were when we
+        % set this up.
+        switch p.Results.whichDirectionNumber
+            case 1
+        end
+    otherwise
+        error('Unknown value for validate type passed.')
+        
 end
-curDir = pwd;
-cd(plotDir);
-
-% Photoreceptor sensitivities
-theFig1 = figure; clf; hold on
-plot(SToWls(S),T_receptors,'LineWidth',2);
-xlabel('Wavelength (nm)')
-ylabel('Sensitivity');
-title('Normalized photoreceptor sensitivities');
-saveas(theFig1,sprintf('%s_%s_%s_Sensitivities.pdf',whichModel,whichPrimaries,whichDirection),'pdf');
-
-% Modulation spectra
-theFig2 = figure; hold on
-plot(SToWls(S),B_primary*modulationPrimary,'r','LineWidth',2);
-plot(SToWls(S),B_primary*backgroundPrimary,'k','LineWidth',2);
-title('Modulation spectra');
-xlim([380 780]);
-xlabel('Wavelength');
-ylabel('Power');
-pbaspect([1 1 1]);
-saveas(theFig2,sprintf('%s_%s_%s_Modulation.pdf',whichModel,whichPrimaries,whichDirection),'pdf');
-
-% Primaries
-theFig3 = figure; hold on
-plot(modulationPrimary,'r','LineWidth',2);
-plot(backgroundPrimary+(-(modulationPrimary-backgroundPrimary)),'g','LineWidth',2);
-plot(backgroundPrimary,'k','LineWidth',2);
-title('Primary settings');
-xlim([0 length(backgroundPrimary)]);
-ylim([0 1]);
-xlabel('Primary Number (nominal)');
-ylabel('Setting');
-legend({'Positive', 'Negative', 'Background'},'Location','NorthEastOutside');
-saveas(theFig3,sprintf('%s_%s_%s_Primaries.pdf',whichModel,whichPrimaries,whichDirection),'pdf');
 
 %% Return to the directory from whence we started
 cd(curDir);
